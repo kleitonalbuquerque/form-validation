f5150f2a05c0ff18e0e275e8ef1236aa2703dd7a
kleitonalbuquerque <kleiton2102@gmail.com>
2022-01-20 18:58:42 -0300

    Validando o formulário

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const session = require("express-session");
 const flash = require("express-flash");
+const cookieParser = require("cookie-parser");
+
 const app = express();
 
 const port = 4400;
@@ -14,52 +16,77 @@ app.use(bodyParser.urlencoded({ extended: false }));
 // parse application/json
 app.use(bodyParser.json());
 
+app.use(cookieParser("fdafasdfasdfasd"));
 app.use(
   session({
     secret: "keyboard cat",
     resave: false,
     saveUninitialized: true,
-    cookie: { secure: true },
+    cookie: { maxAge: 60000 },
   })
 );
 
 app.use(flash());
 
 app.get("/", (req, res) => {
-  res.render("index");
+  var emailError = req.flash("emailError");
+  var pontosError = req.flash("pontosError");
+  var nomeError = req.flash("nomeError");
+  var email = req.flash("email");
+  var nome = req.flash("nome");
+
+  emailError =
+    emailError == undefined || emailError.length == 0 ? undefined : emailError;
+  email = email == undefined || email.length == 0 ? "" : email;
+  nome = nome == undefined || nome.length == 0 ? "" : nome;
+
+  res.render("index", {
+    emailError,
+    pontosError,
+    nomeError,
+    email: email,
+    nome: nome,
+  });
 });
 
 app.post("/form", (req, res) => {
   var { email, nome, pontos } = req.body;
 
   var emailError;
-  var nomeError;
   var pontosError;
+  var nomeError;
 
   if (email == undefined || email == "") {
-    emailError = "E-mail não pode ser vazio";
-  }
-
-  if (nome == undefined || nome == "") {
-    nomeError = "Nome não pode ser vazio";
-  }
-
-  if (nome.length < 4) {
-    nomeError = "O nome deve ter mais que 4 caracteres";
+    emailError = "O e-mail não pode ser vazio";
   }
 
   if (pontos == undefined || pontos < 10) {
     pontosError = "Você não pode ter menos de 10 pontos";
   }
 
+  if (nome == undefined || nome == "") {
+    nomeError = "O nome não pode ser vazio";
+  }
+
+  if (nome.length < 4) {
+    nomeError = "O nome é mt pequeno";
+  }
+
   if (
     emailError != undefined ||
-    nomeError != undefined ||
-    pontosError != undefined
+    pontosError != undefined ||
+    nomeError != undefined
   ) {
+    req.flash("emailError", emailError);
+    req.flash("pontosError", pontosError);
+    req.flash("nomeError", nomeError);
+
+    req.flash("email", email);
+    req.flash("nome", nome);
+
     res.redirect("/");
   } else {
-    res.send("Show de bola esse form!");
+    res.send("SHOW DE BOLA ESSE FORM!");
   }
 });
 
