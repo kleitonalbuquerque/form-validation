088a643fb1946f88f08ccea48a206d791b21bcae
kleitonalbuquerque <kleiton2102@gmail.com>
2022-01-20 17:01:34 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const app = express();
+
+const port = 4400;
+
+app.listen(port, (error) => {
+  if (error) {
+    console.log("Ops! Algo deu errado!");
+  } else {
+    console.log(`Servidor rodando na porta http://localhost:${port}`);
+  }
+});
