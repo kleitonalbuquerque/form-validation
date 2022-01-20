7d4bec4ec35af96a9cf2b14f3c77f19e479e7744
kleitonalbuquerque <kleiton2102@gmail.com>
2022-01-20 17:12:19 -0300

    Preparando base do projeto

--- index.js
+++ index.js
@@ -1,7 +1,33 @@
 const express = require("express");
+const bodyParser = require("body-parser");
+const session = require("express-session");
+const flash = require("express-flash");
 const app = express();
 
 const port = 4400;
+
+app.set("view engine", "ejs");
+
+// parse application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
+
+app.use(
+  session({
+    secret: "keyboard cat",
+    resave: false,
+    saveUninitialized: true,
+    cookie: { secure: true },
+  })
+);
+
+app.use(flash());
+
+app.get("/", (req, res) => {
+  res.send("<h1>Validação de Formulário</h1>");
+});
 
 app.listen(port, (error) => {
   if (error) {
